80b0fc41fb1a0095e728ecdc9c5c8c4e64ca50ae
Totally Tesla <georgeykurien1@gmail.com>
2020-11-28 23:37:48 -0500

    adding lives

--- game.py
+++ game.py
@@ -6,37 +6,56 @@ from random import randint
 # the index always starts at 0
 choices = ["rock","paper","scissors"]
 
-#this is the player's choice
-player = input("Choose rock, paper or scissors: ")
+player_lives = 5
+ai_lives = 5
 
-#this will be the AI choice -> a random pick from the choices in the array
-computer = choices[randint(0, 2)]
 
-#check to see what the user input
+#True and false are boolean data types -> they are the equivalent of on or off, 1 or 0
+player = False
 
-#print outputs whatever is in the round brackets -> in this case it outputs player to the command prompt window
-print("user chose: " + player)
+while player == False:
 
-#validate that the random choice worked for the AI
-print("AI chose: " + computer)
+	#this is the player choice
+	player = input("choose rock, paper or scissors: ")
 
-if (computer == player):
-	print("tie")
+	#this will be the AI choice -> a random pick from the choices in the array
+	computer = choices[randint(0, 2)]
 
-elif (computer == "rock"):
-	if (player == "scissors"):
-	print("you lose!")
-	else:
-		print("you win!")
+	#check to see what the user input
 
-elif (computer == "paper"):
-	if (player == "rock"):
-	print("you lose!")
-	else:
-		print("you win!")
+	#print outputs whatever is in the round brackets -> in this case it outputs player to the command prompt window
+	print("user chose: " + player)
 
-elif (computer == "scissors"):
-	if (player == "paper"):
-	print("you lose!")
-	else:
-		print("you win!")
+	#validate that the random choice worked for the AI
+	print("AI chose: " + computer)
+
+	if (computer == player):
+		print("tie")
+
+	# always check for nagative conditions first (the losing case)
+	elif (computer == "rock"):
+		if (player == "scissors"):
+			print("you lose!")
+			player_lives -= 1
+		else:
+			print("you win!")
+			ai_lives -= 1
+
+	elif (computer == "paper"):
+		if (player == "rock"):
+			print("you lose!")
+			player_lives -= 1
+		else:
+			print("you win!")
+			ai_lives -= 1
+
+	elif (computer == "scissors"):
+		if (player == "paper"):
+			print("you lose!")
+			player_lives -= 1
+		else:
+			print("you win!")
+			ai_lives -= 1
+
+	print("player lives:", player_lives)
+	print("ai lives:", ai_lives)
