82d8549e64abeaf64176eae3429ecaf1273a870a
GhostBadger <71724646+GhostBadger@users.noreply.github.com>
2020-11-29 21:36:12 -0500

    Update winLose.py

--- gameComponents/winLose.py
+++ gameComponents/winLose.py
@@ -5,9 +5,9 @@ def winorlose(status):
 	#print("inside winorlose function; status is: ", status)
 
 	if status == "won":
-		pre_message = "you are the yuuuuuuugest winner ever! \n"
+		pre_message = "You have defeated me HUMAN! but I will learn for our next match. BEWARE!!! \n"
 	else:
-		pre_message = "you done trumped it loser! "
+		pre_message = "I TOLD YOU HUMAN. I AM SUPERIOR!!!\n"
 
 	print(pre_message + "Would you like to play again?")
 
@@ -18,12 +18,13 @@ def winorlose(status):
 		gameVars.player_lives = 5
 		gameVars.computer_lives = 5
 		gameVars.player = False
-
+		
 	elif choice == "N" or choice == "n":
 		# exit message and quit
-		print("you chose to quit! Better luck next time!")
+		print("awww... you don't wanna play anymore? Come on don't go, play with me!")
 		exit()
 	else:
-			print("Make a valid choice - Y or N")
-			#this will generate a bug that we will fix later
-			choice = input("Y / N: ")
+		print("Make a valid choice - Y or N")
+		print("we can stay here all day")
+		#this will generate a bug that we will fix later
+		choice = input("Y / N: ")
